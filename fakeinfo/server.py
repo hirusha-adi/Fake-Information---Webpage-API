4886ef73d9a8d518d7a86f009e13115082482841
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-02 13:30:19 +0530

    code taken from an old project

--- fakeinfo/server.py
+++ fakeinfo/server.py
@@ -15,5 +15,19 @@ else:
 app = Flask(__name__)
 
 
+# / ------------------------------
+@app.route("/", methods=["GET"])
+def index():
+    face = requests.get("https://fakeface.rest/face/json").json()
+    age = face["age"]
+    face_url = face["image_url"]
+
+
+# Errors ------------------------------
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=3331, debug=True)
